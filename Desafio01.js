714b32688965c22363ac827eda55f9793da3c6c7
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2020-06-30 21:40:46 -0300

    Modificado o arquivo Desafio01.js

--- Desafio01.js
+++ Desafio01.js
@@ -1,10 +1,10 @@
 const nome = 'Antonio'
-const peso = 75
-const altura = 1.70
+const peso = 78
+const altura = 1.75
 var IMC = peso / (altura * altura)
 
 if (IMC >= 30 ){
-    console.log(`${nome} você está acima do peso!`)
+    console.log(`${nome}, você está acima do peso!`)
 }else{
-    console.log(`${nome} você não está acima do peso!`)
+    console.log(`${nome}, você não está acima do peso!`)
 }
